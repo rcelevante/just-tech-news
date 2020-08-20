2054f2f495c4c50c3b32b58215010eaea1c7efd0
Rochelle Ruiz <rcelevante@gmail.com>
2020-08-19 18:58:36 -0700

    challenge

--- server.js
+++ server.js
@@ -25,3 +25,19 @@ const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
